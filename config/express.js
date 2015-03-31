478bffd4d3a7ddb5f20a48a5cb44415e5cee64d3
Arinde Eniola <eniola.arinde@andela.co>
2015-03-31 16:16:37 +0100

    made some changes the the package

--- config/express.js
+++ config/express.js
@@ -1,7 +1,21 @@
-var express = require('express');
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 module.exports = function() {
 	var app = express();
+
+	if(process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	}
+
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+	
 	require('../app/routes/tag-routes')(app);
 
 	return app;
