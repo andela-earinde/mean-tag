cc6c2bcd3d098ebb7814c4888643b4358be288ea
Arinde Eniola <eniola.arinde@andela.co>
2015-03-31 17:43:35 +0100

    set up the model with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('./config/express');
+var mongoose = require('./config/mongoose'),
+    express = require('./config/express');
+
+var db = mongoose();
+
+var dbStatus = db.connection;
+
+dbStatus.on('error', console.error.bind(console, 'connection:error'));
 
 var app  = express();
 app.listen(3000, function(){
