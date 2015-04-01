ac682857362c446ae844fd14705b0e2614ad70f2
Arinde Eniola <eniola.arinde@andela.co>
2015-04-01 19:09:02 +0100

    deploying to heroku

--- app/routes/tag-routes.js
+++ app/routes/tag-routes.js
@@ -21,4 +21,9 @@ module.exports = function(app) {
 	.delete(index.deleteTag);
 
 	app.use('/api', router);
+
+	// catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        res.status(404).json({error: "The path does not exist"});
+    });
 }
